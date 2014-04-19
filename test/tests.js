b93e79ce803a658f307b8a7d85b919cf7a5a362b
Krishan Taylor <krishan.taylor@gmail.com>
2014-04-19 17:17:48 -0400

    add stubs for tests

---
+++ test/tests.js
@@ -0,0 +1,24 @@
+/*global describe:false*/
+describe('d3.byte.scale', function () {
+  describe('scale()', function () {});
+
+  describe('#invert()', function () {});
+
+  describe('#domain()', function () {});
+
+  describe('#range()', function () {});
+
+  describe('#rangeRound()', function () {});
+
+  describe('#interpolate()', function () {});
+
+  describe('#clamp()', function () {});
+
+  describe('#nice()', function () {});
+
+  describe('#ticks()', function () {});
+
+  describe('#tickFormat()', function () {});
+
+  describe('#copy()', function () {});
+});
