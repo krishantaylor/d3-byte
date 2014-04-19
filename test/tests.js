1921854911054e7410c9c8505f34271a05b386b0
Krishan Taylor <krishan.taylor@gmail.com>
2014-04-19 18:21:15 -0400

    add a couple of tests
    verify the default values for `#domain()` and `#range()`

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,22 @@
-/*global describe:false*/
+/*global describe:false, it:false, assert:true*/
 describe('d3.byte.scale', function () {
   describe('scale()', function () {});
 
   describe('#invert()', function () {});
 
-  describe('#domain()', function () {});
+  describe('#domain()', function () {
+    it('defaults to [0,1]', function () {
+      var s = d3.byte.scale();
+      assert.deepEqual(s.domain(), [0, 1]);
+    });
+  });
 
-  describe('#range()', function () {});
+  describe('#range()', function () {
+    it('defaults to [0,1]', function () {
+      var s = d3.byte.scale();
+      assert.deepEqual(s.range(), [0, 1]);
+    });
+  });
 
   describe('#rangeRound()', function () {});
 
