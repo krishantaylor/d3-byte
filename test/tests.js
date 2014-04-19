e19c4bb7161e66fd53dd256f9fc646f50318fd68
Krishan Taylor <krishan.taylor@gmail.com>
2014-04-19 18:22:46 -0400

    add pending test for ticks

--- test/tests.js
+++ test/tests.js
@@ -26,7 +26,26 @@ describe('d3.byte.scale', function () {
 
   describe('#nice()', function () {});
 
-  describe('#ticks()', function () {});
+  describe('#ticks()', function () {
+    it.skip('generates ticks of varying degree', function () {
+      var s;
+      s = d3.byte.scale();
+      assert.deepEqual(s.ticks(1), [0, 1]);
+      assert.deepEqual(s.ticks(2), [0, 0.5, 1]);
+      assert.deepEqual(s.ticks(5), [0, 0.25, 0.5, 0.75, 1]);
+      assert.deepEqual(s.ticks(10), [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]);
+      s = d3.byte.scale().domain([1, 0]);
+      assert.deepEqual(s.ticks(1), [0, 1]);
+      assert.deepEqual(s.ticks(2), [0, 0.5, 1]);
+      assert.deepEqual(s.ticks(5), [0, 0.25, 0.5, 0.75, 1]);
+      assert.deepEqual(s.ticks(10), [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]);
+      s = d3.byte.scale().domain([0, 1024]);
+      assert.deepEqual(s.ticks(1), [0, 1024]);
+      assert.deepEqual(s.ticks(2), [0, 512, 1024]);
+      assert.deepEqual(s.ticks(5), [0, 256, 512, 768, 1024]);
+      assert.deepEqual(s.ticks(10), [0, 128, 256, 384, 512, 640, 768, 896, 1024]);
+    });
+  });
 
   describe('#tickFormat()', function () {});
 
